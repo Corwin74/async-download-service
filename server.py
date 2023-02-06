430586f7edb017fed4e256942240ba87393a25dd
Samokhin Alexander <icorwin74@gmail.com>
2023-02-06 20:36:36 +0300

    Step 14

--- server.py
+++ server.py
@@ -37,44 +37,33 @@ async def archive(request):
         stdout=asyncio.subprocess.PIPE,
         cwd=target_directory,
     )
-    request.app['run_procs'][proc.pid] = True
-    i = 0
-    j = []
     try:
         while True:
-            print(proc.stdout.at_eof())
-            print(proc.returncode)
             if proc.stdout.at_eof() and proc.returncode == 0:
                 await response.write_eof()
-                print('All Zip! Send eof')
+                logging.info('Zip OK')
                 break
             if proc.stdout.at_eof():
-                logging.info('Eof bad')
+                logging.info(
+                    'Receive eof, but zip process returncode: %s',
+                    proc.returncode
+                )
                 raise ConnectionResetError
-            if i == 2:
-                #0/0
-                j[3] = 3
-                #raise ZeroDivisionError('Караул!')
             data = await proc.stdout.read(1024*400)
-            logger.info('Sending archive chunk %s bytes to length', len(data))
+            logger.debug('Sending archive chunk %s bytes to length', len(data))
             await response.write(data)
             await asyncio.sleep(1)
-            #i += 1
     except ConnectionResetError:
         logger.info('Download was interrupted')
-    except asyncio.CancelledError:
-        logger.info('Cancelled error exception')
     except SystemExit:
         logger.error('System Exit exception')
     else:
         logging.info('Archive has been sent')
-        request.app['run_procs'].pop(proc.pid, None)
         return response
     finally:
-        print(f"Proc: {request.app['run_procs']}")
         if proc.returncode is None:
             proc.terminate()
-            logging.info('Terminating...')
+            logging.info('Terminating zip process...')
             await proc.communicate()
             logging.info('Zip process has been terminated')
             raise web.HTTPBadRequest(text='Drop connection')
@@ -86,11 +75,6 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
-async def on_shutdown(app):
-    print('Shutdown Aaaaaaaaaaaaa')
-    print(app['run_procs'])
-
-
 def main():
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -98,16 +82,12 @@ def main():
     )
     logger.setLevel(logging.INFO)
     logger.info('Working directory: %s', working_directory)
-    # add stuff to the loop, e.g. using asyncio.create_task()
-    # ...
     app = web.Application()
-    app['run_procs'] = {}
     aiohttp_debugtoolbar.setup(app)
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archive),
     ])
-    app.on_shutdown.append(on_shutdown)
     web.run_app(app)
 
 

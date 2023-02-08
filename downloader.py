29b00b17e8ff92c152d1c998eedda24556969339
Samokhin Alexander <icorwin74@gmail.com>
2023-02-08 15:19:45 +0300

    Downloader release

--- downloader.py
+++ downloader.py
@@ -1,27 +1,37 @@
 import asyncio
-import datetime
+import logging
 import argparse
 import aiohttp
 
 
-async def main(parsed_args):
+logger = logging.getLogger(__file__)
+
+
+async def download(url, filename, pause_at=None, latency=0, delay=None):
     async with aiohttp.ClientSession() as session:
-        async with session.get(parsed_args.url) as resp:
-            with open(parsed_args.filename, 'wb') as file_descriptor:
-                i = 0
+        async with session.get(url) as resp:
+            with open(filename, 'wb') as file_descriptor:
+                logger.info('Start downloading...')
+                iteration_counter = 0
                 async for chunk in resp.content.iter_any():
-                    print(len(chunk))
-                    await asyncio.sleep(parsed_args.latency)
-                    if i == parsed_args.pause_at and parsed_args.delay:
-                        print('Pause....')
-                        print(datetime.datetime.now())
-                        await asyncio.sleep(parsed_args.delay)
+                    logger.debug('Iteration #%s', iteration_counter)
+                    logger.debug('Receive chunk %s bytes length', len(chunk))
+                    await asyncio.sleep(latency)
+                    if iteration_counter == pause_at and delay:
+                        logger.info('Pause for %s seconds', delay)
+                        await asyncio.sleep(delay)
                     file_descriptor.write(chunk)
-                    i += 1
+                    iteration_counter += 1
+            logger.info('Download save to %s', filename)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Downloader')
+def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+    parser = argparse.ArgumentParser()
     parser.add_argument('url')
     parser.add_argument(
         '-f', '--filename',
@@ -31,6 +41,20 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--delay', type=int)
     parser.add_argument('-p', '--pause_at', type=int, default=3)
     parser.add_argument('-l', '--latency', type=float, default=0)
+    parser.add_argument("-v", "--verbose", nargs='?',
+                        const=True, default=False,
+                        help="Activate debug mode.")
     parsed_args = parser.parse_args()
-    print(parsed_args)
-    asyncio.run(main(parsed_args))
+    if parsed_args.verbose:
+        logger.setLevel(logging.DEBUG)
+    asyncio.run(download(
+        parsed_args.url,
+        parsed_args.filename,
+        parsed_args.pause_at,
+        parsed_args.latency,
+        parsed_args.delay,
+    ))
+
+
+if __name__ == '__main__':
+    main()
